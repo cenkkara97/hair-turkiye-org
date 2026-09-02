95e78e72b006d1a0c080c7e17d01237f46090a70
cenkkara97 <59731665+cenkkara97@users.noreply.github.com>
2026-09-02 14:24:30 +0300

    version update 1.1

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,16 @@
 import type { MetadataRoute } from "next";
+
 import { siteConfig } from "@/config/site";
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      disallow: "/",
+      allow: "/",
     },
+
     host: siteConfig.url,
+
+    sitemap: `${siteConfig.url}/sitemap.xml`,
   };
 }
